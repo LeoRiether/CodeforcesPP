8082fad4be5b789f7fac6ed1db018cf92cbfc9c5
Leonardo Riether <leonardoalves8217@gmail.com>
2019-09-02 09:53:11 -0300

    Keep version consistent on all files

--- bundler.js
+++ bundler.js
@@ -2,32 +2,25 @@ const Bundler = require('parcel-bundler');
 const Path = require('path');
 const { promisify } = require('util');
 let fs = require('fs');
-
-// TODO: maybe we don't need this script at all!
+const package = require('./package.json');
 
 let DEV = false; // TODO: change to process.environment
 
 fs.readFileAsync = promisify(fs.readFile);
 fs.writeFileAsync = promisify(fs.writeFile);
 
-let userscriptString = 
-`// ==UserScript==
-// @name         Codeforces++
-// @namespace    cfpp
-// @version      1.0
-// @description  Codeforces extension pack
-// @author       LeoRiether
-// @source       https://github.com/LeoRiether/CodeforcesPP
-// @match        https://codeforces.com/*
-// @grant        none
-// @updateURL    https://github.com/LeoRiether/CodeforcesPP/releases/latest/download/script.meta.js
-// @downloadURL  https://github.com/LeoRiether/CodeforcesPP/releases/latest/download/script.user.js
-// ==/UserScript==\n`;
-// userscriptString = '';
+// Read meta.js and replace {{VERSION}}
+let metaPromise = fs.readFileAsync(Path.join(__dirname, './meta.js'));
+let metaString;
+metaPromise.then(data => {
+    metaString = data.toString().replace("{{VERSION}}", package.version);
+});
 
 const outDir = Path.join(__dirname, './dist');
 const outFilename = 'script.user.js';
+const outMetaFilename = 'script.meta.js';
 const outFile = Path.join(outDir, outFilename); 
+const outMetaFile = Path.join(outDir, outMetaFilename);
 
 let bundler = new Bundler(Path.join(__dirname, "./src/index.js"), {
     outDir:      outDir,
@@ -40,9 +33,12 @@ let bundler = new Bundler(Path.join(__dirname, "./src/index.js"), {
 
 // TODO: Maybe there's a way to pipe Parcel's output to a JS function, then to the output?
 bundler.on('buildEnd', async () => {
+    await metaPromise; // wait for the metaString to be read
+
     let data = await fs.readFileAsync(outFile);
-    data = userscriptString + data;
+    data = metaString + '\n' + data;
     await fs.writeFileAsync(outFile, data);
+    await fs.writeFileAsync(outMetaFile, metaString);
     console.log("[*] UserScript Built!");
 });
 
--- meta.js
+++ meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Codeforces++
 // @namespace    cfpp
-// @version      1.0
+// @version      {{VERSION}}
 // @description  Codeforces extension pack
 // @author       LeoRiether
 // @source       https://github.com/LeoRiether/CodeforcesPP
