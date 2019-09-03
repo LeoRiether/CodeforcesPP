59401b053d02097aee86fc6d86bfa9c8f4468d7e
Leonardo Riether <leonardoalves8217@gmail.com>
2019-09-02 23:21:54 -0300

    Create configuration on localStorage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
+/**
+ * @file Calls the appropriate modules according to the current page
+ */
+
+console.log("Codeforces++ is running!");
+
 let dom = require('./dom');
 
-let problem = require('./problem');
-let problemset = require('./problemset');
-
-require('./style');
-
-if (dom.$('.tag-box')) {
-    problem();
-} else if (dom.$('.problems')) {
-    problemset();
+// Initialize localStorage configuration
+let config;
+function loadConfig() {
+    config = {
+        showTags: true,
+        style:    true,
+        isOk:     true, // In case the config is overwritten by something not-JSON (e.g. a bool), this will tell us
+    };
+    localStorage.cfpp = JSON.stringify(config);
 }
+
+if (!localStorage.cfpp) {
+    loadConfig();
+} else {
+    config = JSON.parse(localStorage.cfpp);
+    if (!config.isOk)
+        loadConfig();
+}
+
+
+if (config.style)
+    require('./style');
+
+// Execute features according to the current page
+if (config.showTags && dom.$('.tag-box')) {
+    require('./show_tags')();
+} else if (config.showTags && dom.$('.problems')) {
+    require('./problemset')();
+}
+
+require('./navbar')();
