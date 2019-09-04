cd78aca24b0bf50ad8952250f193d05998d0a092
Leonardo Riether <leonardoalves8217@gmail.com>
2019-09-04 20:10:24 -0300

    Added the configuration UI (again)

---
+++ src/config.js
@@ -0,0 +1,176 @@
+/**
+ * @file Manages CF++ configuration with localStorage and creates the settings UI
+ */
+
+let dom = require('./dom');
+
+let config = {};
+const defaultConfig = {
+    showTags:  true,
+    style:     true,
+    searchBtn: true,
+    isOk:      true, // In case the config is overwritten by something not-JSON (e.g. a bool), this will tell us
+};
+    
+function load() {
+    config = localStorage.cfpp;
+    config = config ? JSON.parse(config) : false;
+    if (!config || !config.isOk) {
+        reset();
+    }
+}
+
+function reset() {
+    config = defaultConfig;
+    save();
+}
+
+function save() {
+    localStorage.cfpp = JSON.stringify(config);
+}
+
+/**
+ * Creates the interface to change the settings.
+ * The modal created has the following structure:
+ * <div class="cfpp-config cfpp-hidden">
+ *      <div class="cfpp-config-background"></div>
+ *      <div class="cfpp-config-inner">
+ *          <!-- All the settings -->
+ *      </div>
+ * </div>
+ */
+function createUI() {
+    function prop(title, type, id) {
+        return { title, type, id };
+    }
+
+    let modalProps = [
+        prop('"Show Tags" button', 'toggle', 'showTags'),
+        prop('Custom Style', 'toggle', 'style'),
+        prop('"Google It" button', 'toggle', 'searchBtn'),
+    ];
+
+    // Create the actual nodes based on the props
+    modalProps = modalProps.map(p => {
+        if (p.type == 'toggle') {
+            let wrapper = dom.element('div');
+
+            let checkbox = dom.element('input', { 
+                type:     'checkbox',
+                checked:  config[p.id],
+                id:       p.id,
+            });
+            dom.on(checkbox, 'change', () => {
+                // Update property value when the checkbox is toggled
+                config[p.id] = checkbox.checked;
+                save();
+            });
+
+            let label = dom.element('label', { innerText: p.title });
+            label.setAttribute('for', p.id);
+
+            wrapper.appendChild(checkbox);
+            wrapper.appendChild(label);
+            return wrapper;
+        }
+    });
+
+    // Create the modal and its children
+    let modalInner = dom.element('div', { className: 'cfpp-config-inner' , children: modalProps });
+    modalInner.append('Refresh the page to apply changes'); // TODO: not this
+
+    let modalBg = dom.element('div', { className: 'cfpp-config-background' });
+    dom.on(modalBg, 'click', closeUI); // clicking on the background closes the UI
+
+    let modal = dom.element('div', { 
+        className: 'cfpp-config cfpp-hidden',
+        children: [ modalBg, modalInner ]
+    });
+
+    // Create the button that shows the modal
+    let modalBtn = dom.element('a', { 
+        className: 'cfpp-config-btn', 
+        innerText: "[++]"
+    });
+    dom.on(modalBtn, 'click', ev => {
+        ev.preventDefault();
+        modal.classList.remove('cfpp-hidden');
+    });
+
+    // Append the created elements to the DOM
+    document.body.appendChild(modal);
+    dom.$('.lang-chooser').children[0].prepend(modalBtn);
+
+    // TODO: Jesus fucking Christ I need a unified .css file for everything
+    // FIXME: what am I doing
+    // PRIORITY: highest
+    let style = `
+    .cfpp-hidden {
+        display: none;
+        opacity: 0;
+        transition: opacity 0.3s;
+    }
+
+    .cfpp-config-btn {
+        font-size: 22px !important;
+        cursor: pointer;
+    }
+
+    .cfpp-config {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background: #00000087;
+        z-index: 101;
+    }
+    .cfpp-config-background {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background: #00000087;
+    }
+    .cfpp-config-inner {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 50vw;
+        background: white;
+        padding: 2em;
+        border-radius: 6px;
+    }
+    .cfpp-config-inner>div {
+        margin-bottom: 0.5em;
+    }
+
+    .cfpp-config label {
+        margin-left: 0.5em;
+    }
+    `;
+    document.body.appendChild(dom.element('style', {
+        innerHTML: style
+    }));
+}
+
+/**
+ * Toggles the UI's visibility
+ */
+function closeUI() {
+    save();
+    dom.$('.cfpp-config').classList.add('cfpp-hidden');
+}
+
+
+module.exports = {
+    createUI,
+    closeUI,
+    get: key => config[key],
+    set: (key, value) => config[key] = value,
+    load,
+    reset,
+    save,
+};
--- src/index.js
+++ src/index.js
@@ -6,45 +6,29 @@ console.log("Codeforces++ is running!");
 
 let dom = require('./dom');
 
-// Initialize localStorage configuration
-let config;
-function loadConfig() {
-    config = {
-        showTags:  true,
-        style:     true,
-        searchBtn: true,
-        isOk:      true, // In case the config is overwritten by something not-JSON (e.g. a bool), this will tell us
-    };
-    localStorage.cfpp = JSON.stringify(config);
-}
+let config = require('./config');
+config.load();
+config.createUI();
 
-if (!localStorage.cfpp) {
-    loadConfig();
-} else {
-    config = JSON.parse(localStorage.cfpp);
-    if (!config.isOk)
-        loadConfig();
-}
-
-
-if (config.style)
+if (config.get('style'))
     require('./style');
 
 // Execute features according to the current page
-if (config.showTags && dom.$('.tag-box')) {
+if (config.get('showTags') && dom.$('.tag-box')) {
     require('./show_tags')();
-} else if (config.showTags && dom.$('.problems')) {
+} else if (config.get('showTags') && dom.$('.problems')) {
     require('./problemset')();
 }
 
 require('./navbar')();
 
 let searchableRegex = /\/(gym|group)\/(.?)+\/problem\/\w$/i;
-if (config.searchBtn && location.pathname.match(searchableRegex))
+if (config.get('searchBtn') && location.pathname.match(searchableRegex))
     require('./search_button')();
 
+// Exported to a global cfpp variable
 module.exports = {
     debug: {
-        resetConfig: loadConfig,
+        resetConfig: config.reset
     },
 };
