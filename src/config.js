efd6b6a4c6f516aedf81bf69c217f963075cdea2
Leonardo Riether <leonardoalves8217@gmail.com>
2019-09-05 09:40:51 -0300

    Fixes viewport overflow for large tutorials

--- src/config.js
+++ src/config.js
@@ -137,10 +137,12 @@ function createUI() {
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-        width: 50vw;
+        width: 60vw;
+        max-height: 80vh;
         background: white;
         padding: 2em;
         border-radius: 6px;
+        overflow: auto;
     }
     .cfpp-config-inner>div {
         margin-bottom: 0.5em;
