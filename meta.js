18b4613aa3bd8c6d24f9ef1d91e7e0f9ed41952a
Gabriel Taumaturgo <gtaumaturg@gmail.com>
2019-09-05 21:56:53 -0300

    Update meta.js

--- meta.js
+++ meta.js
@@ -5,7 +5,7 @@
 // @description  Codeforces extension pack
 // @author       LeoRiether
 // @source       https://github.com/LeoRiether/CodeforcesPP
-// @match        https://codeforces.com/*
+// @match        *codeforces.com/*
 // @grant        none
 // @updateURL    https://github.com/LeoRiether/CodeforcesPP/releases/latest/download/script.meta.js
 // @downloadURL  https://github.com/LeoRiether/CodeforcesPP/releases/latest/download/script.user.js
