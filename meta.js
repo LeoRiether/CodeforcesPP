a79008cbf5949c7307f8f1abfb57dbd18d49d6bf
Leonardo Riether <leonardoalves8217@gmail.com>
2019-09-26 20:59:46 -0300

    Update meta.js

--- meta.js
+++ meta.js
@@ -5,6 +5,7 @@
 // @description  Codeforces extension pack
 // @author       LeoRiether
 // @source       https://github.com/LeoRiether/CodeforcesPP
+// @icon         https://github.com/LeoRiether/CodeforcesPP/raw/master/cf%2B%2B%20logo.png
 // @match        *codeforces.com/*
 // @grant        none
 // @updateURL    https://github.com/LeoRiether/CodeforcesPP/releases/latest/download/script.meta.js
