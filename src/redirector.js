f3d169eb1fd0599ca05e8272f483c1ba5b80d83b
Leonardo Riether <leonardoalves8217@gmail.com>
2019-10-01 10:33:36 -0300

    Redirect problemset standings and registrants

--- src/redirector.js
+++ src/redirector.js
@@ -24,12 +24,38 @@ function friendsStandings() {
     }
 }
 
+// Redirects contest registrants to friends registrants
+function registrants() {
+    dom.$('.contestParticipantCountLinkMargin').href += '/friends/true';
+}
+
+// Redirects /problemset/standings to the contest standings you actually want
+function problemsetStandings(contestID) {
+    let links = dom.$$('.second-level-menu-list a');
+    for (let link of links) {
+        if (link.href.endsWith('/problemset/standings')) {
+            link.href = link.href.replace('problemset', contestID);
+            break;
+        }
+    }
+}
+
 module.exports = function() {
     if ((/\/groups\/with\//i).test(location.pathname)) {
         groups();
     }
 
+    // Always do this *before* friendsStandings() because of endsWith('/problemset/standings')
+    const contestIDMatch = location.pathname.match(/problemset\/problem\/(\d+)/i);
+    if (contestIDMatch.length > 0) {
+        problemsetStandings(contestIDMatch.groups[1]);
+    }
+
     if (config.get('defStandings') == 'Friends' && !/\/standings/i.test(location.pathname)) {
         friendsStandings();
+    }
+
+    if (dom.$('.contestParticipantCountLinkMargin')) {
+        registrants();
     }
 };
