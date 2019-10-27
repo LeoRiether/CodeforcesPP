44c71ee9ae3d51cd5b083c79e8cbe4d3f8d858f2
Leonardo Riether <leonardoalves8217@gmail.com>
2019-10-27 20:25:09 -0300

    minor changes

--- meta.js
+++ meta.js
@@ -6,7 +6,7 @@
 // @author       LeoRiether
 // @source       https://github.com/LeoRiether/CodeforcesPP
 // @icon         https://github.com/LeoRiether/CodeforcesPP/raw/master/assets/cf%2B%2B%20logo.png
-// @match        *codeforces.com/*
+// @match        *://codeforces.com/*
 // @grant        none
 // @updateURL    https://github.com/LeoRiether/CodeforcesPP/releases/latest/download/script.meta.js
 // @downloadURL  https://github.com/LeoRiether/CodeforcesPP/releases/latest/download/script.user.js
