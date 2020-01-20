93c1b808a6f2080089acef24b6f3d1236f14eec5
Leonardo Riether <leonardoalves8217@gmail.com>
2020-01-19 22:41:03 -0300

    test: Added Funcional.js tests. They can't run.
    Thanks, ES6 import syntax that's not supported by non-bleeding-edge-node versions

--- test/index.js
+++ test/index.js
@@ -1,3 +1,6 @@
+// THIS DOESN'T EVEN RUN ANYMORE
+// TESTS ARE FOR THE WEAK
+
 const test = require('tape');
 const tapDiff = require('tap-diff');
 
@@ -12,7 +15,7 @@ test('Tape works', t => {
 });
 
 test('events.js works', t => {
-  let events = require('../src/events');
+  let events = require('../src/helpers/events');
   t.plan(3);
   events.listen('event_id', data =>
     t.equal(data, 123, "Events fire correctly 1"));
@@ -22,4 +25,33 @@ test('events.js works', t => {
   events.fire('event_id', 123);
   events.fire('creative event name', 'creative data');
   events.fire('event_id', 123);
+});
+
+test('Functional works (but perhaps not all of it, coverage is subpar)', t => {
+  const { curry, pluck } = require('../src/helpers/Functional');
+
+  const sum = curry((x, y) => x + y);
+  t.equal(sum(1, 2), sum (1) (2), `curry`);
+
+  const sym = Symbol();
+  const managers = [
+    { name: 'Bob'          , human: true , [sym]: 1 },
+    { name: 'Alice'        , human: true , [sym]: 2 },
+    { name: 'Tampermonkey' , human: false, [sym]: 3 },
+    { name: 'Violentmonkey', human: false, [sym]: 4 },
+  ];
+
+  t.deepEqual(
+    managers.map (pluck('name')),
+    [ 'Bob', 'Alice', 'Tampermonkey', 'Violentmonkey' ],
+    'basic plucking'
+  );
+
+  t.deepEqual(
+    managers.map (pluck(sym)),
+    [1,2,3,4],
+    'symbol plucking'
+  );
+
+  t.end();
 });
