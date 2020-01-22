9923c661c3c6c2b31e842e863a16a69a247e6919
Leonardo Riether <leonardoalves8217@gmail.com>
2020-01-22 01:22:10 -0300

    contentScript: Run script out of the sandbox

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,1 +1,5 @@
-(typeof browser !== 'undefined' ? browser : chrome).runtime.sendMessage('inject');
+let b = (typeof browser !== 'undefined' ? browser : chrome);
+let script = document.createElement('script');
+script.src = b.runtime.getURL('index.js');
+script.id = 'cfpp-index';
+(document.body || document.head || document.documentElement).appendChild(script);
