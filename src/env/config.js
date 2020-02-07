40f02cb8ddb49f9d2e7c352eb2e9e1f168eed2e1
Leonardo Riether <leonardoalves8217@gmail.com>
2020-02-07 03:12:35 +0000

    config: Decoupled UI

--- src/env/config.js
+++ src/env/config.js
@@ -8,7 +8,7 @@ import dom from '../helpers/dom';
 import { time } from '../helpers/Functional';
 import * as events from '../helpers/events';
 import env from './env';
-import { prop, configProps as modalProps } from './config_props';
+import { Config } from './config_ui';
 
 let config = {};
 const defaultConfig = {
@@ -36,11 +36,9 @@ function reset() {
     config = defaultConfig;
     save();
 }
-
 function save() {
     env.storage.set('cfpp', config);
 }
-
 function commit(id) {
     events.fire(id, config[id]);
     save();
@@ -51,115 +49,23 @@ function commit(id) {
  */
 const createUI = process.env.TARGET == 'extension'
                  ? function(){ /* there's no createUI in extension mode */ }
-                 : env.ready(function() {
+                 : env.ready(function()
+{
     // Some pages, like error pages and m2.codeforces, don't have a header
     // As there's no place to put the settings button, just abort
     if (!dom.$('.lang-chooser')) return;
 
-    if (process.env.NODE_ENV == 'development') {
-        modalProps.push(
-            prop('Version', 'text', 'version')
-        );
+    function pushChange(id, value) {
+        // TODO: fix for extension, maybe
+        config[id] = value;
+        commit(id);
     }
-
-    function makeToggle({id}) {
-        let checkbox =
-            <input id={id}
-                   checked={config[id]}
-                   type="checkbox"/>;
-
-        dom.on(checkbox, 'change', () => {
-            // Update property value when the checkbox is toggled
-            config[id] = checkbox.checked;
-            commit(id);
-        });
-
-        events.listen(id, value => checkbox.checked = value);
-
-        return checkbox;
-    }
-
-    function makeNumber({id}) {
-        let input = <input id={id} value={config[id] || 0} type="number"/>
-
-        dom.on(input, 'input', () => {
-            // Update property value when the number changes
-            config[id] = +input.value;
-            commit(id);
-        });
-
-        return input;
-    }
-
-    function makeSelect({id, data}) {
-        let select = <select id={id}/>;
-        data
-            .map(option =>
-                <option value={option}
-                        selected={option == config[id]}>
-                    {option}
-                </option>
-            )
-            .forEach(opt => select.appendChild(opt));
-
-        dom.on(select, 'change', () => {
-            // Update property value when the number changes
-            config[id] = select.value;
-            commit(id);
-        });
-
-        return select;
-    }
-
-    function makeText({id}) {
-        let input = <input id={id} value={config[id]} type="text"/>
-        dom.on(input, 'change', () => {
-            config[id] = input.value;
-            commit(id);
-        });
-
-        return input;
-    }
-
-    let make = {
-        'toggle': makeToggle,
-        'number': makeNumber,
-        'select': makeSelect,
-        'text':   makeText,
-    };
-
-    // Create the actual nodes based on the props
-    let modalInner = modalProps.map(p => {
-        let node;
-        if (typeof make[p.type] === 'function') {
-            node = make[p.type](p);
-        } else {
-            node = document.createTextNode(`${p.type} does not have a make function! Please check the createUIInner function on config.js`);
-        }
-
-        if (p.type == 'toggle') {
-            // Toggles come before their labels
-            return (
-                <div>
-                    {node}
-                    <label style="margin-left: 0.5em;" for={p.id}>{p.title}</label>
-                </div>
-            );
-        }
-
-        return (
-            <div>
-                <label for={p.id}>{p.title}</label>
-                {node}
-            </div>
-        );
-    });
 
     let modal = (
         <div className="cfpp-config cfpp-modal cfpp-hidden">
             <div className="cfpp-modal-background" onClick={closeUI}/>
             <div className="cfpp-modal-inner">
-                {modalInner}
+                <Config config={config} pushChange={pushChange} pullChange={events.listen} />
             </div>
         </div>
     );
