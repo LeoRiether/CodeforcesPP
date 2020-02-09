702f50dbeca669ddae2157b3343239cf3b29bbc1
Leonardo Riether <leonardoalves8217@gmail.com>
2020-02-09 21:58:26 +0000

    events: Callbacks shouldn't be safe()'d

--- src/helpers/events.js
+++ src/helpers/events.js
@@ -8,7 +8,7 @@ let listeners = {};
 export function listen(event, callback) {
     if (!listeners[event])
         listeners[event] = [];
-    listeners[event].push(safe(callback));
+    listeners[event].push(callback);
 }
 
 export async function fire(event, data) {
